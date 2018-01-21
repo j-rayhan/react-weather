aa6ca45e4c2a522396d32b816b41d33a86ee8771
johir-rayhan <johir.rayhan01@gmail.com>
2018-01-21 20:42:24 +0600

    Update middleware for localhost And add foundation css

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+//load foundation
+
+require('style!css!foundation-sites/dist/foundation.min.css')
+                   //foundation-sites/dist/foundation.min.css
+$(document).foundation();
+
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
